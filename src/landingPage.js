ca274f78f29ba4e4c9e8f98ee216e2b5e06b08f7
kfc0105 <kentam633@gmail.com>
2023-03-04 14:14:11 -0800

    barebone version 2

--- src/landingPage.js
+++ src/landingPage.js
@@ -1,35 +1,53 @@
 import React from 'react';
-import { Typography, Button, Container, Grid } from '@mui/material';
+import { Typography, Button, Container, Grid, AppBar, Toolbar, createTheme, ThemeProvider  } from '@mui/material';
 
-
+const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#006633'
+      }
+    }
+  });
 
 
 const LandingPage = () => {
     return (
-        <div style={{ backgroundColor: '#f8f8f8', padding: '8rem 0' }}>
+        <ThemeProvider theme={theme}>
+        <div>
+          <AppBar position="static">
+            <Toolbar style={{ justifyContent: 'space-between' }}>
+              <Typography variant="h6">
+                CSUS DataFest
+              </Typography>
+              <Button color="inherit">TEMP BUTTON</Button>
+            </Toolbar>
+          </AppBar>
+          <div style={{ backgroundColor: '#f8f8f8', padding: '8rem 0' }}>
             <Container maxWidth="md" align="center">
-                <Typography variant="h1" component="h1" gutterBottom>
-                Welcome to CSUS DATAFEST
-                </Typography>
-                <Typography variant="h5" paragraph>
+              <Typography variant="h1" component="h1" gutterBottom>
+                Welcome to CSUS DataFest
+              </Typography>
+              <Typography variant="h5" paragraph>
                 Hackathon Hub is the go-to place for finding hackathons near you and learning about the latest technologies and trends in the tech industry.
-                </Typography>
-                <div style={{ marginTop: '3rem' }}>
+              </Typography>
+              <div style={{ marginTop: '3rem' }}>
                 <Grid container spacing={2} justify="center">
-                    <Grid item>
+                  <Grid item>
                     <Button variant="contained" color="primary">
-                        Register
+                      Register here !
                     </Button>
-                    </Grid>
-                    <Grid item>
+                  </Grid>
+                  <Grid item>
                     <Button variant="outlined" color="primary">
-                        Learn More
+                      Learn More
                     </Button>
-                    </Grid>
+                  </Grid>
                 </Grid>
-                </div>
+              </div>
             </Container>
+          </div>
         </div>
+      </ThemeProvider>
     );
   };
   
